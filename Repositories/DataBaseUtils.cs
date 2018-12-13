e4c7f54bada3b091dc4b986b68e020769ddc5ef0
Евгений Дисман <zdisman@gmail.com>
2018-12-13 18:25:38 +0300

    Data Base with Users table is added

---
+++ Repositories/DataBaseUtils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Repositories
+{
+    public class DataBaseUtils
+    {
+        private static string path = (System.Environment.CurrentDirectory).Remove(path.IndexOf("\\bin") ,10) + "\\DB.mdf"; //путь к базе данных
+
+
+
+    }
+}
