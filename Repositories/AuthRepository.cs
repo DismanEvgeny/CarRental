e462d8dea5c5549e2f0810c26b38488649b5c564
Georgiy Kobiakov <georgiy.ko@gmail.com>
2018-12-14 21:59:02 +0300

    Working with authentication

---
+++ Repositories/AuthRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IRepositories;
+
+namespace Repositories
+{
+    class AuthRepository : IAuthRepository
+    {
+        string[] IAuthRepository.getUserFromDB(string login)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services/AuthService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Entities;
+using IServices;
+
+namespace Services
+{
+    class AuthService : IAuthService
+    {
+        public bool checkString(string str)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool getUser(AbstractUser user, string login, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
